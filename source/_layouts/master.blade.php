19df1cf1e0b8b2df90e4eb7b14b5d5acd17a020b
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2018-12-18 18:54:43 -0500

    header work

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -6,7 +6,11 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
-    <body>
-        @yield('body')
+    <body class="bg-neutral-lightest text-neutral-darkest">
+        @include('_partials.header')
+
+        <div class="container mx-auto pt-8">
+            @yield('body')
+        </div>
     </body>
 </html>
---
+++ source/_partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <nav class="flex items-center justify-between flex-wrap p-6 shadow-md">
+        <div class="flex flex-col items-left flex-no-shrink">
+            <span class="font-headline font-bold text-3xl text-neutral-lighter">Erica Boarman, OTR/L</span>
+            <span class="font-headline text-xl text-neutral-lighter">Pediatric Occupational Therapist</span>
+        </div>
+        <div class="block lg:hidden">
+            <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            </button>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div class="text-md uppercase font-bold font-body lg:flex-grow lg:flex lg:justify-end">
+                <a href="/services" class="block mt-4 lg:inline-block lg:mt-0 text-neutral-lighter no-underline hover:text-neutral-lightest hover:underline mr-4">
+                    Services
+                </a>
+                <a href="/staff" class="block mt-4 lg:inline-block lg:mt-0 text-neutral-lighter no-underline hover:text-neutral-lightest hover:underline mr-4">
+                    Staff
+                </a>
+                <a href="/contact" class="block mt-4 lg:inline-block lg:mt-0 text-neutral-lighter no-underline hover:text-neutral-lightest hover:underline">
+                    Contact
+                </a>
+            </div>
+        </div>
+    </nav>
+</header>
