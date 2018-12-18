ceb2cf21634a59b7ae3c0bed0a2e01df6b5395cd
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2018-12-17 23:40:46 -0500

    more setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 
+require('laravel-mix-tailwind');
+
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 mix.webpackConfig({
@@ -14,5 +16,7 @@ mix.webpackConfig({
 mix.js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/sass/main.scss', 'css')
     .options({
-        processCssUrls: false,
-    }).version();
+        processCssUrls: false
+    })
+    .tailwind()
+    .version();
